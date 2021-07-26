1ffbf9e43c031099bcb09159eb8a53c08b9a8a41
KarinaLogvina <karinalogvina0@gmail.com>
2021-07-26 22:20:44 +0300

    add: config

---
+++ itra/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Test from './components/Test/Test';
+
+function App() {
+  return (
+    <Test />
+  );
+}
+
+export default App;
---
+++ itra/src/components/Test/Test.js
@@ -0,0 +1,7 @@
+const Test = () => {
+    return (
+        <div>AAAA</div>
+    )
+}
+
+export default Test;
